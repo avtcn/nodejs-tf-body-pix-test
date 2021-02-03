eeee33808ef16e3308e663677f18a700e3a74838
joege <geyijun@gmail.com>
2021-02-03 18:00:29 +0800

    use cdn for resnet50 float version for precise result

--- body-pix/index.js
+++ body-pix/index.js
@@ -7,17 +7,28 @@ function RemoveBackground() {
 
     var bodymodel;
 
+    // https://github.com/tensorflow/tfjs-wechat
+    // use china cdn
     this.loadModel = async function loadModel() {
         if (!this.bodymodel) {
+            // modelUrl: 'https://storage.googleapis.com/tfjs-models/savedmodel/bodypix/mobilenet/quant2/050/model-stride16.json' 
+            // modelUrl: 'https://www.gstaticcnapps.cn/tfjs-models/savedmodel/bodypix/mobilenet/quant2/050/model-stride16.json' 
+            // URL = 'https://www.gstaticcnapps.cn/tfjs-models/savedmodel/bodypix/mobilenet/quant2/050/model-stride16.json';
+
+            URL = 'https://www.gstaticcnapps.cn/tfjs-models/savedmodel/bodypix/resnet50/float/model-stride16.json'
+            // URL = 'https://www.gstaticcnapps.cn/tfjs-models/savedmodel/bodypix/resnet50/quant2/model-stride16.json'
             const resNet = {
-                // architecture: 'ResNet50', // very slow
-                architecture: 'MobileNetV1',
+                architecture: 'ResNet50', // very slow
+                // architecture: 'MobileNetV1',
                 outputStride: 16,
-                multiplier: 1,
-                quantBytes: 2
+                // multiplier: 0.50,
+                quantBytes: 4, // quant4 = float
+                modelUrl: URL
             };
             this.bodymodel = await bodyPix.load(resNet);
             // this.bodymodel = await bodyPix.load();
+            console.log(this.bodymodel);
+
         }
     }
 
@@ -51,8 +62,10 @@ function RemoveBackground() {
 
 ; (async () => {
 
-    const img = fs.readFileSync('ty.jpg');
+    // const img = fs.readFileSync('ty.jpg');
+    const img = fs.readFileSync('images/street.png');
     // const img = fs.readFileSync('images/street2.png');
+    // const img = fs.readFileSync('images/street3.png');
     var convertBG = new RemoveBackground();
 
     let res = await convertBG.removeBG(img, 'output.png');
